23bcc29a1756dd3bb61ed3af21b9d51feedcf25c
Samuel Hernandez Balderas <samuel05.hb@gmail.com>
2025-01-20 11:50:33 -0500

    Added the Experience section with a Vertical Timeline

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,19 +8,19 @@ import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
-    <Tilt className="xs:w-[250px] w-full">
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className="xs:w-[250px] w-full"
+    >
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
-        <div
-          options={{
-            max: 45,
-            scale: 1,
-            speed: 450,
-          }}
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-        >
+        <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
           <img
             src={icon}
             alt="web-development"
@@ -64,4 +64,4 @@ const About = () => {
   );
 };
 
-export const WrappedAbout = SectionWrapper(About, "about");
+export default SectionWrapper(About, "about");
